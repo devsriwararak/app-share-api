6ebc3bedc67b3dd03cef70debfa61892c6b0c0ce
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2024-01-02 21:05:19 +0700

    3

--- db/mysqlConfig.js
+++ db/mysqlConfig.js
@@ -1,10 +1,14 @@
 import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
 
 const pool = mysql.createPool({
-  host: "147.50.231.19",
-  user: "devsriwa_app_share-2",
-  password: "*Nattawut1234",
-  database: "devsriwa_app_share-2",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -13,6 +17,11 @@ const pool = mysql.createPool({
 // host: process.env.DB_HOST,
 // user: process.env.DB_USER,
 // password: process.env.DB_PASSWORD,
-// 1 database: process.env.DB_DATABASE,
+// database: process.env.DB_DATABASE,
+
+// host: "147.50.231.19",
+// user: "devsriwa_app_share-2",
+// password: "*Nattawut1234",
+// database: "devsriwa_app_share-2",
 
 export default pool;
--- index.js
+++ index.js
@@ -18,17 +18,41 @@ import memberRouter from "./Routes/Member.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
+// const pool = mysql.createPool({
+//   host: "147.50.231.19",
+//   user: "devsriwa_app_share-2",
+//   password: "*Nattawut1234",
+//   database: "devsriwa_app_share-2",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
 const pool = mysql.createPool({
-  host: "147.50.231.19",
-  user: "devsriwa_app_share-2",
-  password: "*Nattawut1234",
-  database: "devsriwa_app_share-2",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://app-share-api.vercel.app", 
+      "https://app-share-2.netlify.app"
+    ],
+    methods: ["POST", "GET" , "DELETE", "PUT"],
+
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
